13592ff6b82db4f62b36cf2087200a086e501e15
rptashkin <r.ptashkin@gmail.com>
2020-03-07 16:07:53 -0500

    changes to work on phoenix

--- refit_watcher/refit_watcher.sh
+++ refit_watcher/refit_watcher.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-WATCHER_DIR="/juno/work/ccs/shared/software/refit_watcher/"
+
+WATCHER_DIR="/dmp/hot/ptashkir/lymphoma/copy_number/refit_watcher"
+
 
 INOTIFY_SH="$WATCHER_DIR/bin/refit_watcher_inotify_cmd.sh"
 
 rm -f $INOTIFY_SH
 
-nohup sh -c 'sleep 4; touch $WATCHER_DIR/refit_jobs/facets_refit_cmd_watcher_poll.sh' 2> /dev/null > /dev/null &
+nohup sh -c 'sleep 4; touch $WATCHER_DIR/refit_jobs/facets_refit_cmd_watcher_poll.sh' 2>/dev/null 1>/dev/null &
 
 echo "
-$WATCHER_DIR/bin/inotifywait --format '%w%f' --include 'facets_refit_cmd_.*sh\$' -m -e close_write $WATCHER_DIR/refit_jobs/ | \
+$WATCHER_DIR/bin/inotifywait --format '%w%f' $WATCHER_DIR/refit_jobs -m -e close_write |grep 'facets_cmd_*sh$' --line-buffered | \
   while read FILE
   do
     CUR_TIME=\`date +\"%Y-%m-%d %T\"\`
@@ -18,7 +20,7 @@ $WATCHER_DIR/bin/inotifywait --format '%w%f' --include 'facets_refit_cmd_.*sh\$'
         nohup bash -c 'sleep 59; touch \$WATCHER_DIR/refit_jobs/facets_refit_cmd_watcher_poll.sh' &
     else
         echo \$CUR_TIME Running: \$FILE
-        bash \$FILE > \$FILE.log 2> \$FILE.err &
+        bash \$FILE 1>>$WATCHER_DIR/run_logs/stdout 2>>$WATCHER_DIR/run_logs/stderr &
         echo \$CUR_TIME Finished: \$FILE
     fi
 done " > $INOTIFY_SH
